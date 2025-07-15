235eb5f5d77e36b94fdd5e4cfc650264ecb204cd
LexOnFire <lexgodfire@gmail.com>
2025-07-15 19:50:43 -0300

    alterei algumas coisas mais gerou um erro

--- produtos/admin.py
+++ produtos/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from .models import Produto, Armazem, Estoque
 
-# Register your models here.
+
+
+
+@admin.register(Produto)
+class ProdutoAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'codigo', 'data_validade', 'quantidade', 'vencido', 'perto_vencer')
+    search_fields = ('nome', 'codigo')
+    list_filter = ('data_validade', 'quantidade')
+
+@admin.register(Armazem)
+class ArmazemAdmin(admin.ModelAdmin):
+    list_display = ('rua', 'predio', 'nivel', 'apartamento', 'livre')
+    search_fields = ('rua', 'predio', 'nivel')
+    list_filter = ('livre',)
+
+@admin.register(Estoque)
+class EstoqueAdmin(admin.ModelAdmin):
+    list_display = ('produto', 'armazem', 'quantidade')
+    search_fields = ('produto__nome', 'armazem__rua')
+    list_filter = ('produto', 'armazem')
--- produtos/models.py
+++ produtos/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+from datetime import datetime
 
-# Create your models here.
+class Produto(models.Model):
+    codigo = models.CharField(max_length=50, unique=True)
+    nome = models.CharField(max_length=100)
+    data_validade = models.DateField()
+    quantidade = models.PositiveIntegerField()
+    data_fabricacao = models.DateField(default=datetime.now)
+    lote = models.CharField(max_length=50, blank=True, null=True)
+    
+    def vencido(self):
+        return self.data_validade < datetime.now().date()
+    def perto_vencer(self):
+        return (self.data_validade - datetime.now().date()).days <= 30
+    def __str__(self):
+        return f"{self.nome} (Código: {self.codigo}) - Vencimento: {self.data_validade.strftime('%d/%m/%Y')}"
+
+
+class Armazem(models.Model):
+    livre = models.BooleanField(default=True)
+    rua = models.CharField(max_length=100)
+    predio = models.CharField(max_length=100)
+    nivel = models.CharField(max_length=50)
+    apartamento = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"Rua: {self.rua}, Prédio: {self.predio}, Nível: {self.nivel}, Apartamento: {self.apartamento if self.apartamento else 'N/A'}"
+    
+class Estoque(models.Model):
+    produto = models.ForeignKey(Produto, on_delete=models.CASCADE)
+    armazem = models.ForeignKey(Armazem, on_delete=models.CASCADE)
+    quantidade = models.PositiveIntegerField()
+
+    def __str__(self):
+        return f"{self.produto.nome} - {self.quantidade} unidades em {self.armazem}"
